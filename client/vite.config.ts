87c408d4433ed097c065cdd2b3a26d7fccf87f93
Swarup Sapkota <empswarupcoc@gmail.com>
2024-09-01 09:26:41 +0545

    Vercel route fix #3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   build: {
-    outDir: 'dist', // Ensure the output directory is correct
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   },
-  base: './', // Ensures correct relative paths for assets
-})
+});
